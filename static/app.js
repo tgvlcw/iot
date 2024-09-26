472c6d6553dc015ff9c7d22c1db3dfdce02ea974
Chaowei Liu <tgvlcw@gmail.com>
2024-09-26 09:33:31 +0800

    add new buttons to support more functionalities

--- static/app.js
+++ static/app.js
@@ -7,19 +7,58 @@ const DevicesPage = {
                         <div class="card-body">
                             <h5 class="card-title">{{ device.name }}</h5>
                             <p class="card-text">Status: {{ device.status }}</p>
-                            <div class="btn-group" role="group">
-                                <button @click="toggleDevice(device.id, 'ON')"
-                                        class="btn"
-                                        :class="{'btn-primary': device.status !== 'ON', 'btn-secondary': device.status === 'ON'}"
-                                        :disabled="device.status === 'ON'">
-                                    ON
-                                </button>
-                                <button @click="toggleDevice(device.id, 'OFF')"
-                                        class="btn"
-                                        :class="{'btn-primary': device.status !== 'OFF', 'btn-secondary': device.status === 'OFF'}"
-                                        :disabled="device.status === 'OFF'">
-                                    OFF
-                                </button>
+                            <div class="d-flex align-items-center">
+                                <div class="btn-group" role="group">
+                                    <button @click="toggleDevice(device.id, 'ON')"
+                                            class="btn"
+                                            :class="{'btn-primary': device.status !== 'ON', 'btn-secondary': device.status === 'ON'}"
+                                            :disabled="device.status === 'ON'">
+                                        ON
+                                    </button>
+                                    <button @click="toggleDevice(device.id, 'OFF')"
+                                            class="btn"
+                                            :class="{'btn-primary': device.status !== 'OFF', 'btn-secondary': device.status === 'OFF'}"
+                                            :disabled="device.status === 'OFF'">
+                                        OFF
+                                    </button>
+                                </div>
+                                
+                                <!-- Light Control -->
+                                <div v-if="device.name === 'Light' && device.status === 'ON'" class="ms-3">
+                                    <input type="range" class="form-range" min="0" max="100" v-model="device.brightness" @change="updateBrightness(device.id, device.brightness)">
+                                </div>
+                                
+                                <!-- Fan Control -->
+                                <div v-if="device.name === 'Fan' && device.status === 'ON'" class="ms-3">
+                                    <div class="btn-group" role="group">
+                                        <button v-for="speed in [1, 2, 3]" :key="speed"
+                                                @click="setFanSpeed(device.id, speed)"
+                                                class="btn btn-outline-primary rounded-circle me-1"
+                                                :class="{ active: device.speed === speed }">
+                                            {{ speed }}
+                                        </button>
+                                    </div>
+                                </div>
+                                
+                                <!-- Sound Control -->
+                                <div v-if="device.name === 'Sound' && device.status === 'ON'" class="ms-3">
+                                    <input type="range" class="form-range" min="0" max="100" v-model="device.volume" @change="updateVolume(device.id, device.volume)">
+                                </div>
+                            </div>
+                            
+                            <!-- TV Control -->
+                            <div v-if="device.name === 'TV' && device.status === 'ON'" class="mt-3">
+                                <div class="d-flex justify-content-center">
+                                    <button @click="tvControl(device.id, 'up')" class="btn btn-outline-secondary">↑</button>
+                                </div>
+                                <div class="d-flex justify-content-center align-items-center my-2">
+                                    <button @click="tvControl(device.id, 'left')" class="btn btn-outline-secondary me-2">←</button>
+                                    <button @click="tvControl(device.id, 'ok')" class="btn btn-outline-primary">OK</button>
+                                    <button @click="tvControl(device.id, 'right')" class="btn btn-outline-secondary ms-2">→</button>
+                                </div>
+                                <div class="d-flex justify-content-center">
+                                    <button @click="tvControl(device.id, 'down')" class="btn btn-outline-secondary">↓</button>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -31,6 +70,18 @@ const DevicesPage = {
     methods: {
         toggleDevice(deviceId, targetStatus) {
             this.$emit('toggle-device', deviceId, targetStatus);
+        },
+        updateBrightness(deviceId, brightness) {
+            this.$emit('update-brightness', deviceId, brightness);
+        },
+        setFanSpeed(deviceId, speed) {
+            this.$emit('set-fan-speed', deviceId, speed);
+        },
+        updateVolume(deviceId, volume) {
+            this.$emit('update-volume', deviceId, volume);
+        },
+        tvControl(deviceId, action) {
+            this.$emit('tv-control', deviceId, action);
         }
     }
 };
@@ -101,6 +152,50 @@ new Vue({
     mounted() {
         this.fetchDevices();
         setInterval(this.fetchDevices, 5000); // Fetch every 5 seconds
-    }
+	},
+	updateBrightness(deviceId, brightness) {
+		axios.post('/api/update-brightness', { deviceId, brightness })
+			.then(response => {
+				if (response.data.success) {
+					this.fetchDevices(); 
+				}
+			})
+			.catch(error => {
+				console.error('Error updating brightness:', error);
+			});
+	},
+	setFanSpeed(deviceId, speed) {
+		axios.post('/api/set-fan-speed', { deviceId, speed })
+			.then(response => {
+				if (response.data.success) {
+					this.fetchDevices(); 
+				}
+			})
+			.catch(error => {
+				console.error('Error setting fan speed:', error);
+			});
+	},
+	updateVolume(deviceId, volume) {
+		axios.post('/api/update-volume', { deviceId, volume })
+			.then(response => {
+				if (response.data.success) {
+					this.fetchDevices(); 
+				}
+			})
+			.catch(error => {
+				console.error('Error updating volume:', error);
+			});
+	},
+	tvControl(deviceId, action) {
+		axios.post('/api/tv-control', { deviceId, action })
+			.then(response => {
+				if (response.data.success) {
+					this.fetchDevices(); 
+				}
+			})
+			.catch(error => {
+				console.error('Error controlling TV:', error);
+			});
+	}
 });
 
