4e19faa836aea2095cfefd59fcd19893c96f57cf
Chaowei Liu <tgvlcw@gmail.com>
2024-09-28 09:53:48 +0800

    add get_status call to read device init status

--- hardware.py
+++ hardware.py
@@ -1,21 +1,40 @@
 from mqtt_conf import send_msg
 
+def light_status(device):
+    device['status'] = 'ON'
+    device['brightness'] = 80
+
+def fan_status(device):
+    device['status'] = 'ON'
+    device['speed'] = 2
+
+def tv_status(device):
+    device['status'] = 'ON'
+
+def sound_status(device):
+    device['status'] = 'ON'
+    device['volume'] = 30
+
 def do_light(device, status):
     send_msg(status, device['name'])
     device['status'] = status
+    print(device)
     return True
 
 def do_fan(device, status):
     send_msg(status, device['name'])
     device['status'] = status
+    print(device)
     return True
 
 def do_tv(device, status):
     send_msg(status, device['name'])
     device['status'] = status
+    print(device)
     return True
 
 def do_sound(device, status):
     send_msg(status, device['name'])
     device['status'] = status
+    print(device)
     return True
--- main.py
+++ main.py
@@ -1,28 +1,42 @@
 from flask import Flask, render_template, request, jsonify
-import hardware
+import hardware as hd
 
 app = Flask(__name__)
 
 devices = [
-    {'id': 1, 'name': 'Light', 'status': 'OFF'},
-    {'id': 2, 'name': 'Fan', 'status': 'ON'},
-    {'id': 3, 'name': 'TV', 'status': 'ON'},
-    {'id': 4, 'name': 'Sound', 'status': 'OFF'}
+    {'id': 1, 'name': 'Light', 'status': None, 'brightness': None},
+    {'id': 2, 'name': 'Fan', 'status': None, 'speed': None},
+    {'id': 3, 'name': 'TV', 'status': None},
+    {'id': 4, 'name': 'Sound', 'status': None, 'volume': None}
 ]
 
-def operate_device(device_name, new_status):
+def init_device(device_name, new_status):
     for device in devices:
         if device['name'] == device_name:
             if device_name == 'Light':
-                return hardware.do_light(device, new_status)
+                return hd.do_light(device, new_status)
             elif device_name == 'Fan':
-                return hardware.do_fan(device, new_status)
+                return hd.do_fan(device, new_status)
             elif device_name == 'TV':
-                return hardware.do_tv(device, new_status)
+                return hd.do_tv(device, new_status)
             elif device_name == 'Sound':
-                return hardware.do_sound(device, new_status)
+                return hd.do_sound(device, new_status)
 
     return False
+
+def get_status():
+    for device in devices:
+        if device['name'] == 'Light':
+            hd.light_status(device)
+        elif device['name'] == 'Fan':
+            hd.fan_status(device)
+        elif device['name'] == 'TV':
+            hd.tv_status(device)
+        elif device['name'] == 'Sound':
+            hd.sound_status(device)
+
+    print(devices)
+    return True
 
 @app.route('/api/toggle-device', methods=['POST'])
 def toggle_device():
@@ -30,7 +44,7 @@ def toggle_device():
     device_name = data.get('deviceName')
     new_status = data.get('status')
     #print("Data:", data)
-    if operate_device(device_name, new_status):
+    if init_device(device_name, new_status):
         return jsonify({'success': True})
 
     return jsonify({'success': False}), 404
@@ -41,6 +55,7 @@ def get_devices():
 
 @app.route('/')
 def index():
+    get_status()
     return render_template('index.html', devices=devices)
 
 if __name__ == '__main__':
